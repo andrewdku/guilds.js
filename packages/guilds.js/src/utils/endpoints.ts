1d40e72b6105a2710a65a485530aa69e10194198
andrewdku <notspammail@icloud.com>
2026-02-25 16:27:54 +0200

    docs(endpoints): update jsdoc

--- packages/guilds.js/src/utils/endpoints.ts
+++ packages/guilds.js/src/utils/endpoints.ts
@@ -3,23 +3,23 @@ import { baseApiUrl } from "@/utils";
 /** Discord's API v10 endpoints */
 export const Endpoints = {
     /**
-     * @GET `/gateway`
+     * - GET `/gateway`
      */
     gateway() {
         return `${baseApiUrl}/gateway` as const;
     },
 
     /**
-     * @GET `/gateway/bot`
-     * @POST `/gateway/bot`
+     * - GET `/gateway/bot`
+     * - POST `/gateway/bot`
      */
     gatewayBot() {
         return `${Endpoints.gateway()}/bot` as const;
     },
 
     /**
-     * @GET `/guilds/{guildId}`
-     * @PATCH `/guilds/{guildId}`
+     * - GET `/guilds/{guildId}`
+     * - PATCH `/guilds/{guildId}`
      * @param guildId Guild ID
      */
     guild(guildId: string) {
@@ -27,14 +27,14 @@ export const Endpoints = {
     },
 
     /**
-     * @GET `/guilds/{guildId}`
+     * - GET `/guilds/{guildId}`
      */
     guildPReview(guildId: string) {
         return `${baseApiUrl}/guilds/${guildId}/preview` as const;
     },
 
     /**
-     * @GET `/users/{userId}`
+     * - GET `/users/{userId}`
      * @param userId User ID (default: "@me")
      */
     user(userId: string = "@me") {
