381c6fc657089ec73b730d142386068b40095b5e
andrewdku <notspammail@icloud.com>
2026-02-25 15:23:25 +0200

    chore: export Guild and UnavailableGuild

--- packages/guilds.js/src/classes/index.ts
+++ packages/guilds.js/src/classes/index.ts
@@ -1,5 +1,7 @@
 export * from "./EventHandler";
 export * from "./GuildsError";
 export * from "./RESTManager";
+export * from "./Guild";
+export * from "./UnavailableGuild";
 export * from "./User";
 export * from "./Client";
