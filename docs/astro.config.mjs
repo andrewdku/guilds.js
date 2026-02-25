9d4052b2e4f3cc72be9d9998de65b06c689fc11b
andrewdku <notspammail@icloud.com>
2026-02-25 12:21:11 +0200

    docs: update code theme

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
                 }),
                 rapideTheme(),
             ],
+            expressiveCode: { themes: ["vitesse-dark"] },
             social: [
                 {
                     icon: "github",
