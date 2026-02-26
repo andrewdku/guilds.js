29847d01103eab6f1ab4274bee0fb397445d9db6
andrewdku <notspammail@icloud.com>
2026-02-26 16:07:40 +0200

    fix: export order

--- package/src/index.ts
+++ package/src/index.ts
@@ -8,11 +8,11 @@ export * from "@/utils/endpoints";
 export * from "@/utils/parse-intents";
 
 // Classes
-export * from "@/classes/EventHandler"; // must be declared before client
+export * from "@/classes/EventHandler";
+export * from "@/classes/GuildsError";
+export * from "@/classes/RESTManager";
 export * from "@/classes/Client";
 export * from "@/classes/Guild";
-export * from "@/classes/GuildsError";
 export * from "@/classes/Message";
-export * from "@/classes/RESTManager";
 export * from "@/classes/UnavailableGuild";
 export * from "@/classes/User";
