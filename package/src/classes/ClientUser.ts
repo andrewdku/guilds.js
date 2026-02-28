a90e88e87c0e5beda6965d7cc0c29c34b4cca361
andrewdku <notspammail@icloud.com>
2026-02-28 10:09:11 +0200

    docs(clientuser): update jsdoc

--- package/src/classes/ClientUser.ts
+++ package/src/classes/ClientUser.ts
@@ -2,14 +2,21 @@ import type { APIUser } from "discord-api-types/v10";
 import type { Client } from "@/classes/Client";
 import { User } from "@/classes/User";
 
-/**
- * Class representing the user of the client
- */
+/** Class representing the user of the client */
 export class ClientUser extends User {
+    /** The user's about me (client user only) */
     public bio?: string;
 
+    /**
+     * Instantiate a new client user
+     * @param client Associated client
+     * @param data Discord API user data
+     * @returns ClientUser object
+     */
     public constructor(client: Client, data: APIUser) {
         super(client, data);
         this.bio = (data as any).bio ?? undefined;
+
+        return this;
     }
 }
