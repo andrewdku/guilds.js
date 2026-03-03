b0fda439df12e54d6bdf50141183c5d57aff4ea1
andrewdku <notspammail@icloud.com>
2026-03-03 15:58:59 +0200

    docs: remove confusing jsdoc comments

--- package/src/utils/formatting.ts
+++ package/src/utils/formatting.ts
@@ -3,41 +3,37 @@ export const Formatting = {
     /** Format text as a full block quote covering multiple lines */
     blockQuote: (text: string) => `>>> ${text}` as const,
 
-    /** Format text as **bold** */
+    /** Format text as bold */
     bold: (text: string) => `**${text}**` as const,
 
-    /** Format text as `inline code` */
+    /** Format text as inline code */
     code: (text: string) => `\`${text}\`` as const,
 
     /** Format text as a small gray footer */
     footer: (text: string) => `-# ${text}` as const,
 
-    /** # Format text as an h1 header (largest) */
+    /** Format text as an h1 header */
     h1: (text: string) => `# ${text}` as const,
 
-    /** ## Format text as an h2 header (medium) */
+    /** Format text as an h2 header */
     h2: (text: string) => `## ${text}` as const,
 
-    /** ### Format text as an h3 header (smallest) */
+    /** Format text as an h3 header */
     h3: (text: string) => `### ${text}` as const,
 
-    /** Format text as *italic* */
+    /** Format text as italic */
     italic: (text: string) => `*${text}*` as const,
 
-    /** > Format text as a quote */
+    /** Format text as a quote */
     quote: (text: string) => `> ${text}` as const,
 
-    /** Format text as a ~~strikethrough~~ */
+    /** Format text as strikethrough */
     strikethrough: (text: string) => `~~${text}~~` as const,
 
     /** Format text to be underlined */
     underline: (text: string) => `__${text}__` as const,
 
-    /**
-     *```js
-     * console.log("Format text as a code block, with optional programming language");
-     * ```
-     */
+    /** Format text as a code block, with optional language */
     codeBlock: (text: string, language?: string) =>
         language
             ? (`\`\`\`${language}\n${text}\n\`\`\`` as const)
