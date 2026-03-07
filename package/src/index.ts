5374c772374937c26ffb1bea4ad8c73f67064803
andrewdku <notspammail@icloud.com>
2026-03-07 17:17:52 +0200

    chore: index.ts exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,1 +1,13 @@
-// ...
+// Types
+export type * from "@/types"
+
+// Classes
+export * from "@/classes/event-handler"
+export * from "@/classes/gateway"
+export * from "@/classes/rest-manager"
+
+// Constants
+export * from "@/constants"
+
+// Functions
+export * from "@/functions/parse-token"
