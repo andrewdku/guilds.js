d740c281d111a347a2c39f692a9aee74e4679bbe
coltasa <notspammail@icloud.com>
2026-03-11 13:34:12 +0200

    chore: add discord api to exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,6 @@
 // Types
 export type * from "@/types"
+export type * from "@/discord-api/types"
 
 // Classes
 export * from "@/classes/client"
@@ -9,6 +10,7 @@ export * from "@/classes/rest-manager"
 
 // Constants
 export * from "@/constants"
+export * from "@/discord-api/constants"
 
 // Functions
 export * from "@/functions/parse-token"
