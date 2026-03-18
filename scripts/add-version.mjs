fd83f88380df96df018d3c1e63556239d7128c85
Andrew <notspammail@icloud.com>
2026-03-18 20:09:48 +0200

    ci: update add-version.mjs

--- scripts/add-version.mjs
+++ scripts/add-version.mjs
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-import fs from "node:fs/promises"
+import { appendFile, readFile } from "node:fs/promises"
 import path from "node:path"
 import url from "node:url"
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 const { version } = JSON.parse(
-    await fs.readFile(
+    await readFile(
         path.resolve(__dirname, "..", "packages", "guilds.js", "package.json"),
         "utf8"
     )
 )
 
-await fs.appendFile(
+await appendFile(
     path.resolve(__dirname, "..", "packages", "guilds.js", "dist", "index.cjs"),
     `\nexports.version = "${version}";\n`
 )
 
-await fs.appendFile(
+await appendFile(
     path.resolve(__dirname, "..", "packages", "guilds.js", "dist", "index.d.cts"),
     `\nexport declare const version = "${version}";\n`
 )
 
-console.log(`Version: ${version}`)
+console.log(`Version string: ${version}`)
